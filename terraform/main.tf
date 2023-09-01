05fd9837dc3ae330486f08a7e3c460cdf760b3dc
David Felipe Vanegas Ramirez <david@loka.com>
2023-08-31 20:22:55 -0500

    feat: Add initial code for creating S3 bucket in lakehouse project
    Implemented Terraform configuration to create an S3 bucket that will serve as the foundation for the lakehouse project. The bucket name is generated with a project-specific prefix and a unique random string to ensure global uniqueness.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,9 @@
 provider "aws" {
   region = "us-east-1"  # Change this to your desired region
+  profile = "dvanegas"
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name"  # Choose a unique bucket name
+resource "aws_s3_bucket" "lakehouse_bucket" {
+  bucket = "lakehouse-bucket-dvanegas-${random_id.random_id.hex}"
   acl    = "private"
 }
