6c8c8e1f46b30edfca37d4911a380f48f6cc8ed2
David Felipe Vanegas Ramirez <david@loka.com>
2023-09-01 12:44:10 -0500

    feat: Refactor Glue job to write data in LakeHouse

--- iceberg/job.py
+++ iceberg/job.py
@@ -11,9 +11,10 @@ The mandatory steps are:
 The mandatory job parameters are:
 --warehouse_bucket: The location of the warehouse
 --database_name: The name of the database to create
---table_name: The name of the table to create
 --dummy_data_bucket: The bucket where the dummy data is stored
---dummy_data_s3_key: The path to the dummy data to load into the table
+--dummy_data_key_orders: The key of the dummy data in the S3 bucket
+--dummy_data_key_customers: The key of the dummy data in the S3 bucket
+--dummy_data_key_products: The key of the dummy data in the S3 bucket
 --datalake-formats: The format of the datalake data to load into the table (e.g. Iceberg, delta, hudi)
 """
 
@@ -40,29 +41,26 @@ args = getResolvedOptions(sys.argv, [
     "database_name",
     "table_name",
     "dummy_data_bucket",
-    "dummy_data_s3_key",
+    "dummy_data_key_orders",
+    "dummy_data_key_customers",
+    "dummy_data_key_products",
     ])
 
 # Get the arguments
 warehouse_bucket = args["warehouse_bucket"].strip()
 database_name = args["database_name"].strip()
-table_name = args["table_name"].strip()
 dummy_data_bucket = args["dummy_data_bucket"].strip()
-dummy_data_s3_key = args["dummy_data_s3_key"].strip()
+dummy_data_key_orders = args["dummy_data_key_orders"].strip()
+dummy_data_key_customers = args["dummy_data_key_customers"].strip()
+dummy_data_key_products = args["dummy_data_key_products"].strip()
 
 logger.info(f"Glue Job parameters:\n"
             f"warehouse_location: {warehouse_bucket}\n"
             f"database_name: {database_name}\n"
-            f"table_name: {table_name}\n"
             f"dummy_data_bucket: {dummy_data_bucket}\n"
-            f"dummy_data_s3_key: {dummy_data_s3_key}\n")
-
-conf.set("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
-conf.set("spark.sql.catalog.glue_catalog", "org.apache.iceberg.spark.SparkCatalog")
-conf.set("spark.sql.catalog.glue_catalog.warehouse", f"s3://{warehouse_bucket}/")
-conf.set("spark.sql.catalog.glue_catalog.catalog-impl", "org.apache.iceberg.aws.glue.GlueCatalog")
-conf.set("spark.sql.catalog.glue_catalog.io-impl", "org.apache.iceberg.aws.s3.S3FileIO")
-conf.set("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
+            f"dummy_data_key_orders: {dummy_data_key_orders}\n"
+            f"dummy_data_key_customers: {dummy_data_key_customers}\n"
+            f"dummy_data_key_products: {dummy_data_key_products}\n")
 
 
 def main():
@@ -71,10 +69,10 @@ def main():
     """
     logger.info("Reading the dummy data from the S3 bucket")
     # Read the dummy data from the S3 bucket
-    dummy_data_frame = glue_context.create_dynamic_frame.from_options(
+    data_orders = glue_context.create_dynamic_frame.from_options(
         connection_type="s3",
         connection_options={
-            "paths": [f"s3://{dummy_data_bucket}/{dummy_data_s3_key}"],
+            "paths": [f"s3://{dummy_data_bucket}/{dummy_data_key_orders}"],
             "recurse": True
         },
         format="csv",
@@ -84,21 +82,56 @@ def main():
         }
     ).toDF()
 
-    # dummy_data_frame.createOrReplaceTempView("tmp_dummy_data")
+    data_customers = glue_context.create_dynamic_frame.from_options(
+        connection_type="s3",
+        connection_options={
+            "paths": [f"s3://{dummy_data_bucket}/{dummy_data_key_customers}"],
+            "recurse": True
+        },
+        format="csv",
+        format_options={
+            "withHeader": True,
+            "separator": ","
+        }
+    ).toDF()
+
+    data_products = glue_context.create_dynamic_frame.from_options(
+        connection_type="s3",
+        connection_options={
+            "paths": [f"s3://{dummy_data_bucket}/{dummy_data_key_products}"],
+            "recurse": True
+        },
+        format="csv",
+        format_options={
+            "withHeader": True,
+            "separator": ","
+        }
+    ).toDF()
+
+    # Create temporary views for the dummy data
+    data_orders.createOrReplaceTempView("tmp_dummy_data_orders")
+    data_customers.createOrReplaceTempView("tmp_dummy_data_customers")
+    data_products.createOrReplaceTempView("tmp_dummy_data_products")
 
     logger.info("Appending the dummy data to the Iceberg table")
     # Append the dummy data to the Iceberg table using Data Catalog
-    # query = f"""
-    # CREATE TABLE glue_catalog.{database_name}.{table_name}
-    # USING iceberg
-    # AS SELECT * FROM tmp_dummy_data
-    # """
+    query = f"""
+        INSERT INTO glue_catalog.{database_name}.orders
+        SELECT * FROM tmp_dummy_data_orders
+    """
+    spark.sql(query)
 
-    spark.sql(f"CREATE DATABASE IF NOT EXISTS glue_catalog.{database_name}")
+    query = f"""
+        INSERT INTO glue_catalog.{database_name}.customers
+        SELECT * FROM tmp_dummy_data_customers
+    """
+    spark.sql(query)
 
-    dummy_data_frame.writeTo(f"glue_catalog.{database_name}.{table_name}").tableProperty('format-version', '2').append()
-
-    job.commit()
+    query = f"""
+        INSERT INTO glue_catalog.{database_name}.products
+        SELECT * FROM tmp_dummy_data_products
+    """
+    spark.sql(query)
 
 
 if __name__ == "__main__":
